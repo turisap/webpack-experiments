4cde1b1d1cb842cf797e1c9277e8003a5e5c1b3f
turisap <kirillshakirov57@gmail.com>
2019-09-11 05:54:00 -0400

    css and style loaders

--- build-utils/webpack.development.js
+++ build-utils/webpack.development.js
@@ -1,1 +1,11 @@
-module.exports = () => ({});
+module.exports = () => ({
+    module : {
+        rules : [
+            {
+                test: /\.css/,
+                use : ['style-loader', 'css-loader']
+            }
+        ]
+
+    }
+});
